6b3351eb871012becfc1413744fef7635c5fc060
Finu <finnantiku@gmail.com>
2025-06-09 17:15:45 +0300

    created account.cpp based on  tests.cpp and a log file

---
+++ CPP_00/ex02/Account.cpp
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsolomon <fsolomon@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/09 14:15:46 by fsolomon          #+#    #+#             */
+/*   Updated: 2025/06/09 17:09:15 by fsolomon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account() {}
+
+Account::Account( int initial_deposit ): _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
+{
+	_totalAmount += initial_deposit;
+	_accountIndex = _nbAccounts++;
+	//[19920104_091532] index:0;amount:42;created
+	std::cout << "index:" << _accountIndex
+			<< ";amount:" << _amount
+			<< ";created" << std::endl;
+}
+Account::~Account( void )
+{
+	//[19920104_091532] index:0;amount:47;closed
+	std::cout << "index:" << _accountIndex
+			<< ";amount:" << _amount
+			<< ";closed" << std::endl;
+}
+
+int	Account::getNbAccounts( void ) {return _nbAccounts;}
+int	Account::getTotalAmount( void ) {return _totalAmount;}
+int	Account::getNbDeposits( void ) {return _totalNbDeposits;}
+int	Account::getNbWithdrawals( void ) {return _totalNbWithdrawals;}
+
+void	Account::displayAccountsInfos( void )
+{
+	//[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
+	std::cout << "accounts:" << _nbAccounts
+			<< ";total:" << _totalAmount
+			<< ";deposits:" << _totalNbDeposits
+			<< ";withdrawals:" << _totalNbWithdrawals
+			<< std::endl;
+}
+void	Account::makeDeposit( int deposit )
+{
+	_amount += deposit;
+	_totalAmount += deposit;
+	_nbDeposits++;
+	_totalNbDeposits++;
+
+	//[19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
+	std::cout << "index:" << _accountIndex
+			<< ";p_amount:" << _amount - deposit
+			<< ";deposit:" << deposit
+			<< ";amount:" << _amount
+			<< ";nb_deposits:" << _nbDeposits
+			<< std::endl;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	if (withdrawal > checkAmount())
+	{
+	//[19920104_091532] index:0;p_amount:47;withdrawal:refused
+		std::cout << "index:" << _accountIndex
+				<< ";p_amount:" << _amount
+				<< ";withdrawal:refused"
+				<< std::endl;
+		return false;
+	}
+	else
+	{
+		_amount -= withdrawal;
+		_totalAmount -= withdrawal;
+		_nbWithdrawals++;
+		_totalNbWithdrawals++;
+
+	//[19920104_091532] index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1
+		std::cout << "index:" << _accountIndex
+				<< ";p_amount:" << _amount + withdrawal
+				<< ";withdrawal:" << withdrawal
+				<< ";amount:" << _amount
+				<< ";nb_withdrawals:" << _nbWithdrawals
+				<< std::endl;
+	}
+	return true;
+}
+
+int		Account::checkAmount( void ) const
+{
+	return _amount;
+}
+
+void	Account::displayStatus( void ) const
+{
+	//[19920104_091532] index:0;amount:47;deposits:1;withdrawals:0
+	std::cout << "index:" << _accountIndex
+			<< ";amount:" << _amount
+			<< ";deposits:" << _nbDeposits
+			<< ";withdrawals:" << _nbWithdrawals
+			<< std::endl;
+}
+
+void	Account::_displayTimestamp( void )
+{
+
+}
