cdbfc843e9c66c645caeb4bfd4fafa3e42f35df8
Finu <finnantiku@gmail.com>
2025-06-11 09:43:38 +0300

    created needed files for ex03

---
+++ CPP_01/ex03/Weapon.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsolomon <fsolomon@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 09:36:50 by fsolomon          #+#    #+#             */
+/*   Updated: 2025/06/11 09:42:29 by fsolomon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_H
+#define WEAPON_H
+
+#include <iostream>
+
+class Weapon
+{
+	public:
+
+	private:
+		std::string _type;
+};
+
+#endif
