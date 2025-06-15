49145c830cdc5a5b651df698af519882fc397eb0
Finu <finnantiku@gmail.com>
2025-06-15 17:47:23 +0300

    handle replacement on the full string content instead of line by line

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -6,29 +6,28 @@
 /*   By: fsolomon <fsolomon@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 18:53:54 by fsolomon          #+#    #+#             */
-/*   Updated: 2025/06/15 17:24:53 by fsolomon         ###   ########.fr       */
+/*   Updated: 2025/06/15 17:46:20 by fsolomon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
-std::string &checkLine(std::string &line, const std::string &toBeReplaced, const std::string &newStr)
+std::string &replaceContent(std::string &content, const std::string &toBeReplaced, const std::string &newStr)
 {
-	if (toBeReplaced.empty())
-		return line;
-
 	size_t pos = 0;
-	while ((pos = line.find(toBeReplaced, pos)) != std::string::npos)
+
+	while ((pos = content.find(toBeReplaced, pos)) != std::string::npos)
 	{
 		std::cout << "searched STR found at pos " << pos << std::endl;
 		// here we should replace the stuff!
-		line.erase(pos, toBeReplaced.length());
-		line.insert(pos, newStr);
+		content.erase(pos, toBeReplaced.length());
+		content.insert(pos, newStr);
 		pos = pos + newStr.length();
 	}
-	std::cout << "Line AFTER Replacing " << line << std::endl;
-	return line;
+	std::cout << "AFTER Replacing " << content << std::endl;
+	return content;
 }
 int main(int argc, char **argv)
 {
@@ -37,27 +36,42 @@ int main(int argc, char **argv)
 		std::cout << "Usage: ./sed <filename> <str1> <str2>" << std::endl;
 		return 1;
 	}
+
 	std::string toBeReplaced = argv[2];
 	std::string newStr = argv[3];
-	std::string fileName = argv[1] + std::string(".replace");
+	std::string fileName = argv[1];
+	std::string newFileName = fileName + ".replace";
 
-	std::ifstream ifs;
-	std::ofstream ofs;
-
-	ifs.open(argv[1]);
-	ofs.open(fileName);
-
-	if (!ifs.is_open() || !ofs.is_open())
+	if (toBeReplaced.empty())
 	{
-		std::cout << "Error with file!" << std::endl;
+		std::cout << "Nothing to be replaced in here." << std::endl;
 		return 1;
 	}
-	std::string line;
-	while (std::getline(ifs, line))
+
+	std::ifstream ifs(fileName);
+	if (!ifs.is_open())
 	{
-		checkLine(line, toBeReplaced, newStr);
-		ofs << line;
+		std::cout << "Error opening " << fileName << std::endl;
+		return 1;
 	}
+
+	std::stringstream buffer;
+	std::string fileContent;
+	buffer << ifs.rdbuf();
+	fileContent = buffer.str();
+
+	replaceContent(fileContent, toBeReplaced, newStr);
+
+	std::ofstream ofs(newFileName);
+	if (!ofs.is_open())
+	{
+		ifs.close();
+		std::cout << "Error creating " << newFileName << std::endl;
+		return 1;
+	}
+
+	ofs << fileContent;
+	ifs.close();
 	ofs.close();
 	return 0;
 }
