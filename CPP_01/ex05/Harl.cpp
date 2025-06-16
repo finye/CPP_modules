cb726adebe0ebaf53afd64fa99dece675e992257
Finu <finnantiku@gmail.com>
2025-06-16 11:33:24 +0300

    uses funct ptrs to access non static member functions

--- CPP_01/ex05/Harl.cpp
+++ CPP_01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: fsolomon <fsolomon@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 00:11:53 by fsolomon          #+#    #+#             */
-/*   Updated: 2025/06/16 01:01:59 by fsolomon         ###   ########.fr       */
+/*   Updated: 2025/06/16 11:33:11 by fsolomon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,36 @@
 
 void Harl::complain(std::string level)
 {
-	// TODO: call the functions according to level
+	std::string commentLevels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	void (Harl::*funcPtrs[4])(void) = {&Harl::_debug,
+									   &Harl::_info,
+									   &Harl::_warning,
+									   &Harl::_error};
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == commentLevels[i])
+			(this->*funcPtrs[i])();
+	}
 }
+
 void Harl::_debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << "[DEBUG] I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 void Harl::_info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money."
+	std::cout << "[INFO] I cannot believe adding extra bacon costs more money."
 			  << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
 			  << std::endl;
 }
 void Harl::_warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free."
+	std::cout << "[WARNING] I think I deserve to have some extra bacon for free."
 			  << "I’ve been coming for years whereas you started working here since last month."
 			  << std::endl;
 }
 void Harl::_error(void)
 {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ERROR] This is unacceptable! I want to speak to the manager now." << std::endl;
 }
--- CPP_01/ex05/main.cpp
+++ CPP_01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fsolomon <fsolomon@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 00:12:12 by fsolomon          #+#    #+#             */
-/*   Updated: 2025/06/16 01:02:14 by fsolomon         ###   ########.fr       */
+/*   Updated: 2025/06/16 11:31:44 by fsolomon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,7 @@ int main()
 {
 	Harl harl;
 	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
 }
