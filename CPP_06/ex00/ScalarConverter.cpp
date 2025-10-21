53019214132d4a7378925e10fc2bb4713e780f8d
fsolomon <finnantiku@gmail.com>
2025-10-21 12:30:27 +0300

    private helper functs created WIP

--- CPP_06/ex00/ScalarConverter.cpp
+++ CPP_06/ex00/ScalarConverter.cpp
@@ -5,5 +5,13 @@ void	ScalarConverter::convert(const std::string &input)
 	//first detect the type of the input passed
 	//covert it to its actual type
 	//then convert it explicitly to the other datatypes • char • int • float • double
-	(void) input;
+	if (isChar(input))
+	{
+
+	}
 }
+
+bool	ScalarConverter::isChar(const std::string &input){}
+bool	ScalarConverter::isInt(const std::string &input){}
+bool	ScalarConverter::isFloat(const std::string &input){}
+bool	ScalarConverter::isDouble(const std::string &input){}
--- CPP_06/ex00/ScalarConverter.hpp
+++ CPP_06/ex00/ScalarConverter.hpp
@@ -9,5 +9,10 @@ class ScalarConverter
 		ScalarConverter(const ScalarConverter &src) = delete;
 		ScalarConverter& operator=(const ScalarConverter &rhs) = delete;
 
-		static void	convert(const std::string &original);
+		static void	convert(const std::string &input);
+	private:
+		static bool isChar(const std::string &input);
+		static bool isInt(const std::string &input);
+		static bool isFloat(const std::string &input);
+		static bool isDouble(const std::string &input);
 };
