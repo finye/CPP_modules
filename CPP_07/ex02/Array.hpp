98f2f2d06695912f0dfff577903e48657d50eb71
Finu <finnantiku@gmail.com>
2025-10-29 17:01:45 +0200

    adds const version of [] operator, for values passed as const ref, remove nullcheck before delete[]

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -6,37 +6,36 @@ class Array
 {
 	public:
 		Array(): _n(0), _arr(nullptr){}
-		Array(unsigned int n): _n(n), _arr(new T[n]){};
-		~Array()
-		{
-			if (_arr != nullptr)
-				delete[] _arr;
-		}
-		Array(const Array& src): _n(src._n), _arr(new T[src._n])
+		Array(unsigned int n): _n(n), _arr(n > 0 ? new T[n] : nullptr){}
+		~Array() { delete[] _arr; }
+		Array(const Array& src): _n(src._n), _arr(src._n > 0 ? new T[src._n] : nullptr)
 		{
 			for (unsigned int i = 0; i < src._n; i++)
 				_arr[i] = src._arr[i];
 		}
 		Array &operator =(const Array &rhs)
 		{
-			if (this == rhs)
+			if (this == &rhs)
 				return *this;
 
-			if (_arr)
-			{
-				delete[] _arr;
-				_arr = nullptr;
-			}
+			delete[] _arr;
 			_n = rhs._n;
 
-			_arr = new T[rhs._n];
-			for (unsigned int i = 0; i < rhs._n; i++)
+			_arr = (_n > 0) ? new T[_n] : nullptr;
+			for (unsigned int i = 0; i < _n; i++)
 				_arr[i] = rhs._arr[i];
 
 			return *this;
 		}
 
-		T& operator [](unsigned int n)
+		T& operator[](unsigned int n)
+		{
+			if (n >= _n)
+				throw std::out_of_range("Error: index is out of range");
+			return _arr[n];
+		}
+
+		const T& operator[](unsigned int n) const
 		{
 			if (n >= _n)
 				throw std::out_of_range("Error: index is out of range");
