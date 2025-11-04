a355be022950917639df4569b4be34e5050c156e
Finu <finnantiku@gmail.com>
2025-11-04 14:38:06 +0200

    implements mutant stack class which adds an iterable functionality to an std::stack class

---
+++ CPP_08/ex02/main.cpp
@@ -0,0 +1,61 @@
+#include "MutantStack.hpp"
+#include <iostream>
+#include <list>
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << "Top: "<< mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << "Size: " << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+
+	std::cout << "Stack contents:" << std::endl;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	std::cout <<"Top: " << s.top() << std::endl;
+
+	std::cout << "\n___std::list for comparison___" << std::endl;
+
+	std::list<int> lst;
+
+	lst.push_back(5);
+	lst.push_back(17);
+
+	std::cout << "Back: " << lst.back() << std::endl;
+	lst.pop_back();
+
+	std::cout << "Size: " << lst.size() << std::endl;
+
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	lst.push_back(0);
+
+	std::list<int>::iterator lit = lst.begin();
+	std::list<int>::iterator lite = lst.end();
+
+	++lit;
+	--lit;
+
+	std::cout << "List contents:" << std::endl;
+	while (lit != lite)
+	{
+		std::cout << *lit << std::endl;
+		++lit;
+	}
+	return 0;
+}
